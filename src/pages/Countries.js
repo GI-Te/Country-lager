29dacdab69d9b22cfa555f2a7d6ce057e9c295a5
GI-Te <asiyananjo@gmail.con>
2021-03-29 10:14:39 -0700

    warnings cleared

--- src/pages/Countries.js
+++ src/pages/Countries.js
@@ -3,21 +3,17 @@ import Card from "../components/Card";
 import Loading from "../components/Loading";
 import Error from "../components/Error";
 import { numberWithCommas } from "../utils/numbersComma";
-import { useState } from "react";
 
 function Countries() {
   const url = "https://restcountries.eu/rest/v2/all";
   const { countries, isPending, error } = useCountries(url);
-  // const [search,setSearch]=useState('')
 
   return (
     <section className="mt-5 bg-info">
       <div className="container">
         <div className="m-5 border-bottom bg-transparent text-center">
           <h1>List Of countries</h1>
-        </div>
-            {/* <input type="text" placeholder="search" onChange={e=>setSearch(e.target.value)}></input> */}
-        
+        </div>        
         <div className="row text-center">
           {isPending && <Loading />}
           {error && <Error />}
